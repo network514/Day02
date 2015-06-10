ccf8d6b0e6c92d4aaa5d6151b9911e8afa62e8d3
JAVA_0608 <JAVA_0608@ITB401-PC>
2015-06-10 09:17:55 +0900

    Day02

---
+++ Day02/src/FirstECL.java
@@ -0,0 +1,15 @@
+
+public class FirstECL {
+	// 이클립스에서 컴파일 하는 방법
+	// => 저장하면 자동으로 컴파일이 됨 
+	// => .class파일이 생성됨. 
+	// => 단축키 : 
+	// ctrl + s - 현재 파일만 저장
+	// ctrl + shift + s - 프로젝트 모든 파일 동시 저장
+	// 실행 : run 버튼 클릭
+
+	public static void main(String[] args) {
+		System.out.println("이클립스 시작");
+	}
+
+}
